e11470a087aa5d97169e8eaec03d03c3f50b98d0
aRbhaSun20 <remo78295279@gmail.com>
2022-05-28 23:38:45 +0530

    changes in ui

--- src/Context/Locations.js
+++ src/Context/Locations.js
@@ -14,7 +14,9 @@ export const useLocationQuery = () => {
             _id
             placeName
             coordinates
-            userId
+            user {
+              name
+            }
           }
       }`,
       });
