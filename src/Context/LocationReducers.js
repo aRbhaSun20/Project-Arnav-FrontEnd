dadaa1b223ffc33af9ed1398514cea6cf1bb66c4
ashwin chakravarthy <ashwinchakravarthy2000@gmail.com>
2022-06-20 13:15:19 +0530

    QR

--- src/Context/LocationReducers.js
+++ src/Context/LocationReducers.js
@@ -9,7 +9,13 @@ export const LocationReducers = (InitialState = getDAta(), actions) => {
       const data = { ...InitialState, ...actions.payload };
       localStorage.setItem("arnavLocation", JSON.stringify(data));
       return data;
-
+    case LOCATION_ACTIONS.Default_location:
+      localStorage.removeItem("arnavLocation");
+      return {
+        parentId: "",
+        fromId: "",
+        toId: "",
+      };
     default:
       return InitialState;
   }
